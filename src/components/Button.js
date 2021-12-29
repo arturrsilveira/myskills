5eec2ff2525cf217cc187c31ab402c9e279a2b62
Artur <arturrsilveira85@gmail.com>
2021-12-29 14:59:11 -0300

    Envio Inicial

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { 
+    TouchableOpacity, 
+    Text,
+    StyleSheet
+} from 'react-native';
+
+export function Button({ onPress }){
+    return(
+        <TouchableOpacity 
+            style={styles.button} 
+            activeOpacity={.7}
+            onPress={onPress}
+        >
+                <Text style={styles.buttonText}>
+                Add
+                </Text>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 17
+      },
+    
+      button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+      }
+})
