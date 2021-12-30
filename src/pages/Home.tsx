d52f386799f7578cbe86cd9e97253ba65d779722
Artur <arturrsilveira85@gmail.com>
2021-12-29 23:14:12 -0300

    Aplicaçao Refatorada para TypeScript e Finalizada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,29 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+    id: string;
+    name: string;
+    date?: Date;
+}
+
 export function Home(){
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [gretting, setGrettig] = useState('');
 
   function handleAddNewSkill(){
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+        id: String(new Date().getTime()),
+        name: newSkill
+    }
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+        skill => skill.id !== id)
+    )
   }
 
   useEffect(() => {
@@ -37,21 +53,24 @@ export function Home(){
   return (
     <View style={styles.container}>
       
-      <Text style={styles.title}>
+    <Text style={styles.title}>
         Welcome, Artur Rafael
-      </Text>
+    </Text>
 
-      <Text style={styles.greetings}>
+    <Text style={styles.greetings}>
         {gretting}
-      </Text>
+    </Text>
 
-      <TextInput style={styles.input} 
+    <TextInput style={styles.input} 
         placeholder="New skill" 
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
-      />
+    />
 
-      <Button onPress={handleAddNewSkill} />
+    <Button 
+        onPress={handleAddNewSkill}
+        title={'Add'} 
+    />
 
       <Text style={[styles.title, { marginVertical:15 }]}>
         My Skills
@@ -59,9 +78,12 @@ export function Home(){
 
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item}/>
+          <SkillCard 
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
       )}
      />
 
